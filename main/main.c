db5f2b6ec128ebde4e34e85aa7e770773df698e9
Krishna-13-cyber <krishnanarayanan132002@gmail.com>
2024-03-15 21:05:26 +0530

    Add BLE setup as component

--- main/main.c
+++ main/main.c
@@ -5,27 +5,30 @@
 
 #include "mpu.h"
 #include "sdkconfig.h"
+#include "esp_hid_gap.h"
+#include "esp_hid_device.h"
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 
-static const char *TAG = "mpu_example";
+static const char *TAG = "MAIN";
 
 void app_main()
 {
+    int16_t acce_raw_value[BUFF_SIZE / 2], gyro_raw_value[BUFF_SIZE / 2];
     while (1)
     {
-    // Enable the MPU-6050 Sensor and check for error
+      // Enable the MPU-6050 Sensor and check for error
       if (enable_mpu() == ESP_OK)
       {
           // Setting up the result arrays, time difference and initial conditions
-          int16_t acce_raw_value[BUFF_SIZE / 2], gyro_raw_value[BUFF_SIZE / 2];
+          setup_ble(); // Setup BLE
           while (read_mpu_raw(acce_raw_value, gyro_raw_value) == ESP_OK)
           {
-            ESP_LOGI(TAG, "acce_raw_value = %d %d %d",acce_raw_value[0],acce_raw_value[1],acce_raw_value[2]);
+            if (ble_hid_task(acce_raw_value) != ESP_OK)
+              ESP_LOGE(TAG, "%s", "Failed to send acc values !!");
           }
-        }
+      }
     }
-
     ESP_LOGE(TAG, "%s", "Error reading values!");
 }
